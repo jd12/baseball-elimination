b17a95388ddc176366d6366c20d736076ee8f361
J.D. DeVaughn-Brown <jd@cs.umass.edu>
2018-02-18 11:04:50 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## Assignment Specification
+http://coursera.cs.princeton.edu/algs4/assignments/seam.html
+
+## Checklist 
+http://coursera.cs.princeton.edu/algs4/checklists/seam.html
+
---
+++ src/test/java/BaseballEliminationTest.java
@@ -0,0 +1,95 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author <insert your name hre>
+ *
+ */
+public class BaseballEliminationTest {
+
+  /**
+   * @throws java.lang.Exception
+   */
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  /**
+   * Test method for {@link BaseballElimination#BaseballElimination(java.lang.String)}.
+   */
+  @Test
+  public void testBaseballElimination() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  /**
+   * Test method for {@link BaseballElimination#numberOfTeams()}.
+   */
+  @Test
+  public void testNumberOfTeams() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  /**
+   * Test method for {@link BaseballElimination#teams()}.
+   */
+  @Test
+  public void testTeams() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  /**
+   * Test method for {@link BaseballElimination#wins(java.lang.String)}.
+   */
+  @Test
+  public void testWins() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  /**
+   * Test method for {@link BaseballElimination#losses(java.lang.String)}.
+   */
+  @Test
+  public void testLosses() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  /**
+   * Test method for {@link BaseballElimination#remaining(java.lang.String)}.
+   */
+  @Test
+  public void testRemaining() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  /**
+   * Test method for {@link BaseballElimination#against(java.lang.String, java.lang.String)}.
+   */
+  @Test
+  public void testAgainst() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  /**
+   * Test method for {@link BaseballElimination#isEliminated(java.lang.String)}.
+   */
+  @Test
+  public void testIsEliminated() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  /**
+   * Test method for {@link BaseballElimination#certificateOfElimination(java.lang.String)}.
+   */
+  @Test
+  public void testCertificateOfElimination() {
+    fail("Not yet implemented"); // TODO
+  }
+
+}
